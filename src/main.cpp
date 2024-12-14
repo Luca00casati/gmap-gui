bb533a8649e6050be940658479e94ed5943c4b55
Luca Casati <luk00casati@gmail.com>
2024-12-15 00:30:09 +0100

    implement dialog

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
+using namespace std;
 
-//#include <nfd.h>
+#include <nfd.h>
 
 #include <GLFW/glfw3.h>
 #include <imgui.h>
@@ -11,34 +12,57 @@
 
 #define GLSL_VERSION "#version 330"
 
-int main() {    
+nfdchar_t* chosefile() {
+    nfdchar_t *outPath = NULL;
+    nfdresult_t result = NFD_OpenDialog(NULL, NULL, &outPath);
+
+    if (result == NFD_OKAY) {
+        //puts("Success!");
+        //puts(outPath);
+        return outPath;
+    } else if (result == NFD_CANCEL) {
+        //puts("User pressed cancel.");
+        return NULL;
+    } else {
+        printf("Error: %s\n", NFD_GetError());
+    }
+    return NULL;
+}
+
+int main()
+{
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    GLFWmonitor* primaryMonitor = glfwGetPrimaryMonitor();
-    const GLFWvidmode* mode = glfwGetVideoMode(primaryMonitor);
+    GLFWmonitor *primaryMonitor = glfwGetPrimaryMonitor();
+    const GLFWvidmode *mode = glfwGetVideoMode(primaryMonitor);
 
     GLFWwindow *window = glfwCreateWindow(mode->width, mode->height, "Hello, ImGUI!",
-                                        nullptr, nullptr);
-    if (window == nullptr) {
-        std::cout << "Could not create GLFW window" << std::endl;
+                                          nullptr, nullptr);
+    if (window == nullptr)
+    {
+        cout << "Could not create GLFW window" << endl;
         glfwTerminate();
-        return -1;
+        return 1;
     }
     glfwMakeContextCurrent(window);
 
     ImGui::CreateContext();
 
-    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    ImGuiIO &io = ImGui::GetIO();
+    (void)io;
     io.IniFilename = nullptr;
-    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
+    // io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
     ImGui_ImplGlfw_InitForOpenGL(window, true);
     ImGui_ImplOpenGL3_Init(GLSL_VERSION);
 
-    while (!glfwWindowShouldClose(window)) {
+    nfdchar_t *file = nullptr;
+
+    while (!glfwWindowShouldClose(window))
+    {
         glfwPollEvents();
 
         glClearColor(0, 0, 0, 0);
@@ -48,15 +72,31 @@ int main() {
         ImGui_ImplGlfw_NewFrame();
         ImGui::NewFrame();
 
-        ImGui::ShowDemoWindow(nullptr);
+        // ImGui::ShowDemoWindow(nullptr);
 
-        /*
         ImGui::Begin("Window");
         {
             ImGui::Text("Hello, Dear ImGUI!");
+            if (ImGui::Button("file"))
+            {
+                if (file)
+                {
+                    free(file);
+                    file = nullptr;
+                }
+                file = chosefile();
+            }
+
+            if (file == nullptr)
+            {
+                ImGui::Text("No file chosen");
+            }
+            else
+            {
+                ImGui::Text("Chosen file: %s", file);
+            }
         }
         ImGui::End();
-        */
 
         ImGui::Render();
         ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
